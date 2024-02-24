a5af28c1fcca6d8645efb5b8c84e34c74d84a938
bcad2130 <brettcadigan@gmail.com>
2024-02-23 22:22:47 -0500

    Overworld Battle Preview TG-69 #done

--- Overworld/BattleSpawner.cs
+++ Overworld/BattleSpawner.cs
@@ -1,10 +1,22 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class BattleSpawner : MonoBehaviour
 {
     public string sceneName;
     public int stageClearNumber;
+    public GameObject previewWindow;
+    public GameObject unlockIcon;
+    public GameObject lockIcon;
+    public GameObject startBattleLabel;
+    public GameObject cannotStartBattleLabel;
+    public GameObject enemyPreviewText;
+
+    private GameObject previewObj;
+    private GameObject lockObj;
+    private GameObject startBattleObj;
+    private GameObject enemyTextObj;
 
     [SerializeField]
     private GameObject[] enemyEncounterPrefabs;
@@ -13,9 +25,11 @@ public class BattleSpawner : MonoBehaviour
     private GameObject dialogue;
 
     private StatusTracker tracker;
+    private Canvas canvas;
 
     private bool spawning = false;
     private bool dialoguing = false;
+    public bool collision = false;
 
     void Start() {
         DontDestroyOnLoad (this.gameObject);
@@ -23,6 +37,7 @@ public class BattleSpawner : MonoBehaviour
         SceneManager.sceneLoaded += OnSceneLoaded;
 
         tracker = GameObject.FindObjectOfType<StatusTracker>();
+        canvas = GameObject.FindObjectOfType<Canvas>();
     }
 
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode) {
@@ -40,7 +55,6 @@ public class BattleSpawner : MonoBehaviour
             }
 
             if (this.dialoguing && dialogue) {
-                // Debug.Log("spawn dialogue");
                 Instantiate (dialogue);
             }
             SceneManager.sceneLoaded -= OnSceneLoaded;
@@ -48,17 +62,60 @@ public class BattleSpawner : MonoBehaviour
         }
     }
 
-    void OnTriggerEnter2D(Collider2D other) {
+    private void OnTriggerEnter2D(Collider2D other) {
         if (other.gameObject.tag == "OverworldPlayer") {
-            if (tracker.GetStageClearTotal() + 1 == stageClearNumber) {
-                LoadBattleScene();
+            collision = true;
+            SpawnPreviewWindow();
+        }
+    }
+
+    private void SpawnPreviewWindow() {
+        previewObj = Instantiate (previewWindow, canvas.transform);
+        previewObj.transform.position = this.transform.position;
+
+        if (tracker.GetStageClearTotal() >= stageClearNumber - 1) {
+            lockObj = Instantiate (unlockIcon, previewObj.transform);
+            startBattleObj = Instantiate (startBattleLabel, previewObj.transform);
+        } else {
+            lockObj = Instantiate (lockIcon, previewObj.transform);
+            startBattleObj = Instantiate (cannotStartBattleLabel, previewObj.transform);
+        }
+
+        if (enemyEncounterPrefabs.Length > 0) {
+            enemyTextObj = Instantiate (enemyPreviewText, previewObj.transform);
+            TMP_Text testText = enemyTextObj.GetComponent<TMP_Text>();
+
+            foreach(GameObject enemy in enemyEncounterPrefabs) {
+                UnitStats unit = enemy.GetComponent<UnitStats>();
+
+                testText.text += unit.GetName() + "\n";
             }
         }
     }
 
+    private void OnTriggerExit2D(Collider2D other) {
+        collision = false;
+
+        DespawnPreviewWindow();
+    }
+
+    private void DespawnPreviewWindow() {
+        Destroy(previewObj);
+        Destroy(lockObj);
+        Destroy(startBattleObj);
+        Destroy(enemyTextObj);
+    }
+
     void LoadBattleScene() {
-        this.spawning = true;
-        this.dialoguing = true;
         SceneManager.LoadScene(sceneName);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Return) && collision == true && tracker.GetStageClearTotal() >= stageClearNumber - 1)
+        {
+            this.spawning = true;
+            this.dialoguing = true;
+            LoadBattleScene();
+        }
     }
 }
